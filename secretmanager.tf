e9015601e94faf4e8b296dd3832660009a83d014
Sourav Kundu <skundu.dev@gmail.com>
2023-04-03 21:33:10 -0500

    added terraform configuration

---
+++ secretmanager.tf
@@ -0,0 +1,24 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/secretsmanager_secret
+resource "aws_secretsmanager_secret" "secret_one" {
+  name = "secure_secret_one"
+  recovery_window_in_days = 0
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/secretsmanager_secret_version
+resource "aws_secretsmanager_secret_version" "secure_one_version" {
+  secret_id     = aws_secretsmanager_secret.secret_one.id
+  secret_string = var.SomeOtherSecret 
+  #The value is passed to the Terraform via the CLI
+}
+
+resource "aws_secretsmanager_secret" "db_secrets" {
+  name = "environment/secrets"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_secrets_version" {
+  secret_id = aws_secretsmanager_secret.db_secrets.id
+  secret_string = <<EOF
+  {"username": "${var.username}","password":"${var.password}"}
+  EOF
+  #The value of the username and password are passed to Terraform via the CLI
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+#Define AWS Region
+variable "region" {
+  description = "Infrastructure region"
+  type        = string
+  default     = "us-east-2"
+}
+#Define IAM User Access Key
+variable "access_key" {
+  description = "The access_key that belongs to the IAM user"
+  type        = string
+  sensitive   = true
+}
+#Define IAM User Secret Key
+variable "secret_key" {
+  description = "The secret_key that belongs to the IAM user"
+  type        = string
+  sensitive   = true
+}
+#SecretManager secrets variables
+variable "username" {
+  description = "The username of the entity"
+  type = string
+  sensitive = true
+}
+variable "password" {
+  description = "The password of the entity"
+  type = string
+  sensitive = true
+}
+variable "SomeOtherSecret" {
+  description = "Some other secret"
+  type = string
+  sensitive = true
+}
