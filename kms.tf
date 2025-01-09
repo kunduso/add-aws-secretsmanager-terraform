6b2659ff78f22ab6983e38b968d371a76269fa8c
Sourav Kundu <skundu.dev@gmail.com>
2025-01-08 18:00:15 -0600

    fix for #31

--- kms.tf
+++ kms.tf
@@ -19,6 +19,15 @@ resource "aws_kms_key_policy" "encrypt_kms" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
         Sid    = "Allow access through AWS Secrets Manager for all principals in the account that are authorized to use AWS Secrets Manager"
         Effect = "Allow"
         Principal = {
