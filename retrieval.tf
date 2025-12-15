67dab6a36077f86f3751bea65f3ceab70a172ec5
Sourav Kundu <skundu.dev@gmail.com>
2025-12-15 16:24:20 -0600

    address race condition and added retrieval mechanism #37

--- retrieval.tf
+++ retrieval.tf
@@ -3,4 +3,12 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/ephemeral-resources/secretsmanager_secret_version
 ephemeral "aws_secretsmanager_secret_version" "write_only_retrieved" {
   secret_id = aws_secretsmanager_secret.write_only_secret.id
+  depends_on = [ aws_secretsmanager_secret_version.write_only_version ]
 }
+
+resource "aws_ssm_parameter" "retrieved_secret" {
+  name  = "/database/password/master"
+  type  = "SecureString"
+  value_wo = ephemeral.aws_secretsmanager_secret_version.write_only_retrieved.secret_string
+  value_wo_version = 1
+  }
