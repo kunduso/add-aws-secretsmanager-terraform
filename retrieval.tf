a56f143aa78c84f9c4b9925640198c186fb7fa31
Sourav Kundu <skundu.dev@gmail.com>
2025-12-15 17:01:28 -0600

    fix format

--- retrieval.tf
+++ retrieval.tf
@@ -2,13 +2,13 @@
 # Retrieve the stored database credentials
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/ephemeral-resources/secretsmanager_secret_version
 ephemeral "aws_secretsmanager_secret_version" "write_only_retrieved" {
-  secret_id = aws_secretsmanager_secret.write_only_secret.id
-  depends_on = [ aws_secretsmanager_secret_version.write_only_version ]
+  secret_id  = aws_secretsmanager_secret.write_only_secret.id
+  depends_on = [aws_secretsmanager_secret_version.write_only_version]
 }
 
 resource "aws_ssm_parameter" "retrieved_secret" {
-  name  = "/${var.name}/retrieved_secret"
-  type  = "SecureString"
-  value_wo = ephemeral.aws_secretsmanager_secret_version.write_only_retrieved.secret_string
+  name             = "/${var.name}/retrieved_secret"
+  type             = "SecureString"
+  value_wo         = ephemeral.aws_secretsmanager_secret_version.write_only_retrieved.secret_string
   value_wo_version = 1
-  }
+}
