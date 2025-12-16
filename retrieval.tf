a75f8de218947f54e32b0b3c654dbb4d77d9f0a6
Sourav Kundu <skundu.dev@gmail.com>
2025-12-16 14:36:16 -0600

    added encryption to ssm parameter

--- retrieval.tf
+++ retrieval.tf
@@ -6,8 +6,11 @@ ephemeral "aws_secretsmanager_secret_version" "write_only_retrieved" {
   depends_on = [aws_secretsmanager_secret_version.write_only_version]
 }
 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
 resource "aws_ssm_parameter" "retrieved_secret" {
   name             = "/${var.name}/retrieved_secret"
+  description      = "Retrieved write-only secret stored in SSM Parameter Store"
+  key_id           = aws_kms_key.local_key.arn
   type             = "SecureString"
   value_wo         = ephemeral.aws_secretsmanager_secret_version.write_only_retrieved.secret_string
   value_wo_version = 1
